a5d374b6f3853eb5baa74b6fe46343932a6f7d5b
ArtyomTrubachev <artyom.trubachev@gmail.com>
2022-04-06 19:01:01 +0300

    UI-1/Task/Registration-authorization/Only window auth without registration and without dynamic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {Route, RouterModule} from "@angular/router";
+import { RegistrationComponent } from './registration/registration.component';
+
+const routes: Route[] = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
