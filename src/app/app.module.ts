4456349b800e00aa5e789fee8100e9849c0ccb93
skymarce <duraev-andrey2014@yandex.ru>
2022-04-08 12:14:55 +0300

    UI-3/Task/Сontent-for-main-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { GoodsComponent } from './goods/goods.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    GoodsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit, AfterViewInit {
+  isActive = false;
+  @ViewChild('searchInput') searchInput: ElementRef;
+  catalogMenu = [
+    {name: 'Гироскутеры', src: '../../assets/icons/catalog/hydro_scooter.svg'},
+    {name: 'Электросамокаты', src: '../../assets/icons/catalog/electric_scooter.svg'},
+    {name: 'Моноколеса', src: '../../assets/icons/catalog/unicycle.svg'},
+    {name: 'Сигвеи и мини-сигвеи', src: '../../assets/icons/catalog/segway.svg'},
+    {name: 'Электроскутеры', src: '../../assets/icons/catalog/electric_scooter_2.svg'},
+    {name: 'Электровелосипеды', src: '../../assets/icons/catalog/electric_bike.svg'},
+    {name: 'Электроскейты', src: '../../assets/icons/catalog/electric_skateboard.svg'},
+    {name: 'Электромобили', src: '../../assets/icons/catalog/electric_car.svg'},
+    {name: 'Аксессуары', src: '../../assets/icons/catalog/accessories.svg'},
+    {name: 'Умные игрушки', src: '../../assets/icons/catalog/smart_toy.svg'},
+    {name: 'Smart Watch', src: '../../assets/icons/catalog/smart_watch.svg'}
+  ];
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.search()
+  }
+
+  ngOnInit(): void { }
+
+  public toCatalogSection(event: Event) {
+    event.stopPropagation()
+    console.log(event.target)
+  }
+
+  public search() {
+    fromEvent(this.searchInput.nativeElement, 'input')
+      .pipe(
+        filter(Boolean),
+        debounceTime(1500),
+        distinctUntilChanged(),
+        tap(
+          (response) => {
+            console.log(this.searchInput.nativeElement.value);
+          }
+        )
+      ).subscribe();
+  }
+
+}
